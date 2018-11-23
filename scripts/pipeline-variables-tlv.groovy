a1c87cfb4010f9531687b275a619acf221463092
dccohee <calecohee@gmail.com>
2018-11-23 15:23:24 -0500

    testing string concatenation in groovy script

--- scripts/pipeline-variables-tlv.groovy
+++ scripts/pipeline-variables-tlv.groovy
@@ -1,1 +1,4 @@
-env.TEST = pwd() 
+env.WORKSPACE_DIR = pwd()
+env.APP_NAME = "tlv"
+env.OMAR_COMMON_PROJ_NAME = "omar-common"
+env.TEST = "$env.WORKSPACE_DIR/$OMAR_COMMON_PROJ_NAME/omar-common-properties.gradle"
