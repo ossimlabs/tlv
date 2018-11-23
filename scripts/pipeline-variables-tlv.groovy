255c0ef1dc9db928649a23e0feef449d8a722df5
dccohee <calecohee@gmail.com>
2018-11-23 15:40:12 -0500

    removing all local variables

--- scripts/pipeline-variables-tlv.groovy
+++ scripts/pipeline-variables-tlv.groovy
@@ -1,4 +1,4 @@
 env.WORKSPACE_DIR = pwd()
 env.APP_NAME = "tlv"
 env.OMAR_COMMON_PROJ_NAME = "omar-common"
-env.TEST = "$env.WORKSPACE_DIR/$OMAR_COMMON_PROJ_NAME/omar-common-properties.gradle"
+env.OMAR_COMMON_PROPERTIES = "$env.WORKSPACE_DIR/$OMAR_COMMON_PROJ_NAME/omar-common-properties.gradle"
