65c2a3bdcd681c1cd27f2f3b8ec013c41a61cac6
dccohee <calecohee@gmail.com>
2018-12-05 10:42:19 -0500

    updating pipeline variable script

--- scripts/pipeline-variables-tlv.groovy
+++ scripts/pipeline-variables-tlv.groovy
@@ -1,7 +1,7 @@
-env.GIT_BRANCH = "dev"
-env.GIT_PUBLIC_URL = "git@github.com:ossimlabs"
-env.WORKSPACE_DIR = pwd()
-env.APP_NAME = "tlv"
-env.OMAR_COMMON_PROJ_NAME = "omar-common"
+env.OSSIM_GIT_BRANCH = "dev"
+env.GIT_PUBLIC_SERVER_URL = "git@github.com:ossimlabs"
+env.workspaceDir = pwd()
+env.appName = "tlv"
+env.omarCommonProjName = "omar-common"
 env.OMAR_COMMON_PROPERTIES = "$env.WORKSPACE_DIR/$OMAR_COMMON_PROJ_NAME/omar-common-properties.gradle"
 env.O2_DEV_HOME=pwd()
