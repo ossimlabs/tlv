6196149e3f1f764f8c6a51879a8224e9d2f3cc5f
Cale Cohee <calecohee@gmail.com>
2018-12-05 10:44:36 -0500

    Fixing variable names

--- scripts/pipeline-variables-tlv.groovy
+++ scripts/pipeline-variables-tlv.groovy
@@ -3,5 +3,5 @@ env.GIT_PUBLIC_SERVER_URL = "git@github.com:ossimlabs"
 env.workspaceDir = pwd()
 env.appName = "tlv"
 env.omarCommonProjName = "omar-common"
-env.OMAR_COMMON_PROPERTIES = "$env.WORKSPACE_DIR/$OMAR_COMMON_PROJ_NAME/omar-common-properties.gradle"
+env.OMAR_COMMON_PROPERTIES = "$env.workspaceDir/$env.omarCommonProjName/omar-common-properties.gradle"
 env.O2_DEV_HOME=pwd()
