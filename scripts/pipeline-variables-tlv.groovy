745e672f915ca25ea92e09a66e98bbe7d07d6eab
dccohee <calecohee@gmail.com>
2018-12-10 14:41:26 -0500

    Load common variable

--- scripts/pipeline-variables-tlv.groovy
+++ scripts/pipeline-variables-tlv.groovy
@@ -1,9 +1,5 @@
-env.OSSIM_GIT_BRANCH = "dev"
-env.GIT_PUBLIC_SERVER_URL = "git@github.com:ossimlabs"
-env.workspaceDir = pwd()
 env.appName = "tlv"
+env.workspaceDir = pwd()
 env.omarCommonProjName = "omar-common"
 env.OMAR_COMMON_PROPERTIES = "$env.workspaceDir/$env.omarCommonProjName/omar-common-properties.gradle"
 env.O2_DEV_HOME = pwd()
-env.SONARQUBE_NAME = "sonar_cloud"
-env.HP_FORTIFY_URL = "http://hpfortify.ossim.io:8080/ssc"
