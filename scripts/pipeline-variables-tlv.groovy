2dd88c6771ea3f27e202e1e053b971c23252bb91
dccohee <calecohee@gmail.com>
2018-12-12 15:31:18 -0500

    removing testing

--- scripts/pipeline-variables-tlv.groovy
+++ scripts/pipeline-variables-tlv.groovy
@@ -3,4 +3,3 @@ env.workspaceDir = pwd()
 env.omarCommonProjName = "omar-common"
 env.OMAR_COMMON_PROPERTIES = "$env.workspaceDir/$env.omarCommonProjName/omar-common-properties.gradle"
 env.O2_DEV_HOME = pwd()
-env.test = "test1"
