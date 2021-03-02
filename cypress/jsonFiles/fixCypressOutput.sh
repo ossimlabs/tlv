30da39a68744940e4bcb8946b2b1ad92d525c6b5
coryrobertsonmaxar <65191048+coryrobertsonmaxar@users.noreply.github.com>
2021-03-02 12:10:38 -0500

    Update fixCypressOutput.sh

--- cypress/jsonFiles/fixCypressOutput.sh
+++ cypress/jsonFiles/fixCypressOutput.sh
@@ -1,7 +1,7 @@
-echo '{ "Links": [' > links.json
-cat test_.json >> links.json
-echo "]}" >> links.json
-sed "s+}{+},{+g" links.json
+echo '{ "Links": [' > links2.json
+cat test_.json >> links2.json
+echo "]}" >> links2.json
+sed "s+}{+},{+g" links2.json > links.json
 
 # rm test_.json
 # rm links.json.txt
