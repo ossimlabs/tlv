9b2e2ee5a91acdd1165fef8f5bafc2e0c09e2927
Cory Robertson <coryrobertson@Corys-MacBook-Pro.local>
2021-03-03 01:51:45 -0500

    changes

--- cypress/comparison.py
+++ cypress/comparison.py
@@ -59,46 +59,70 @@ if (go == 0):
 
 	iter = 0
 	partB = {}
-	i = 0
-	for item in result:
-		# print("\n\n\n")
 
+	keyList = []
+	subKeyList = []
+	for param in params:
+		for key, value in param.items():
+			subKeyList.append(key)
+		keyList.append(subKeyList)
+		subKeyList = []
+
+
+	for i in range(0, len(result)):
+		# print("..........")
 		tempDict = {}
-		for key, value in item.items():
-			if key == "format":
-				break
-			tempDict[key] = value
+		for j in range(0, len(keyList[i])):
+
+			# partB[testNames[i]] =
+			tempDict[keyList[i][j]] = result[i][keyList[i][j]]
 		partB[testNames[i]] = tempDict
-		i = i + 1
+	# print("PartA\n...........")
+	# print(partA)
+	# print("PartB\n...........")
+	# print(partB)
 
 
-	print(partB)
+	# use this to cycle through each param in each keyList
+	# for test in keyList:
+	# 	print(".........\n")
+	# 	for param in test:
+	# 		print(param)
+
+
+
+
+	# iter = 0
+	# partB = {}
+	# for item in result:
+	# 	keyList = list(item.items())
+	# 	resultParams = {}
+	# 	for item in keyList:
+	# 		if (item[0] == 'format'):
+	# 			continue
+	# 		resultParams[str(item[0])] = str(item[1])
+	# 	resultParamsList = []
+	# 	resultParamsList.insert(len(resultParamsList), resultParams)
+	# 	partB[testNames[iter]] = resultParamsList
+	# 	iter = iter + 1
 	#
-	# keyList = []
-	# subKeyList = []
-	# for param in params:
-	# 	for key, value in param.items():
-	#  		subKeyList.append(key)
-	# 	keyList.append(subKeyList)
-	# 	subKeyList = []
-	#
-	#
-	# for i in range(0, len(result)):
-	# 	tempDict = {}
-	# 	for j in range(0, len(keyList[i])):
-	# 		tempDict[keyList[i][j]] = result[i][keyList[i][j]]
-	# 	partB[testNames[i]] = tempDict
-	#
-	#
+
 	q = open("finalResult.json", "w")
 	q.write("{")
 
+	#
+	# print("\n\n.......................")
+	# print(partA)
+	# print("......................\n\n")
+	#
 	for name in testNames:
 		q.write('\n\t"' + str(name) + '" : ')
 
 		if partA[name] == partB[name]:
 			q.write("true")
 			print(name + " test PASSED")
+			print(partA[name])
+			print(partB[name])
 		else:
 			q.write("false")
 			print(name + " test FAILED")
